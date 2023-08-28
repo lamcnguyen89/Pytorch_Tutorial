64b99fea670e926e173e9c44d79ae567fd454146
Lam C Nguyen <lamcnguyen89@gmail.com>
2023-08-28 18:34:18 -0400

    Initial Test file for the pytorch tutorial

---
+++ 01_Test.py
@@ -0,0 +1,22 @@
+# File to test if the Virtual Environment and Packages are all working correctly:
+
+"""
+    1. First create the environment in the terminal using the command:
+            conda create --name environment_name
+            
+    2. Next activate the environment using the command:
+            conda activate environment_name
+            
+    3. To deactivate environment, use the command:
+            conda deactivate
+            
+    4. Since I'm using visual studio code, I need to change the interpreter to match the virtual environment I created in Anaconda
+
+"""
+
+
+import torch
+
+print(torch.__version__)
+x = torch.rand(64,18)
+print(x)
