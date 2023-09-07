640ded2ef26bd4527ac04182097ff1b3a6faa64b
Lam C Nguyen <lamcnguyen89@gmail.com>
2023-09-06 22:13:42 -0400

    Finished tutorial on basic CNN

--- 06_Convolutional_Neural_Network.py
+++ 06_Convolutional_Neural_Network.py
@@ -1,7 +1,9 @@
 # =====================================================#
-#             Pytorch Neural Network Example           #
+#     Pytorch Convolutional Neural Network Example     #
 # =====================================================#
-# From Aladdin Persson video entitled: Complete Pytorch Tensor Tutorial(Pytorch Neural Network Example)
+# From Aladdin Persson video entitled: Pytorcg CNN Example  (Convolutional Neural Network)
+
+# Convolutional Neural Networks work better on images then fully connected networks.
 
 import torch
 import torch.nn as nn # All the Neural network models, loss functions
@@ -10,35 +12,59 @@ import torch.nn.functional as F # All functions without parameters
 from torch.utils.data import DataLoader # Easier dataset management such as minibatches
 import torchvision.datasets as datasets # Standard datasets that can be used as test training data
 import torchvision.transforms as transforms # Transformations that can be performed on the dataset
+from tqdm import tqdm # For progress bar
 
+# Create Convolutional Neural Network
 
-# Create Fully Connected Neural Network
-class NN(nn.Module):
-    def __init__(self,input_size, num_classes):
-        super(NN, self).__init__() # The Super keyword calls the initialization of the parent class
-        self.fc1 = nn.Linear(input_size, 50) # Create a small NN
-        self.fc2 = nn.Linear(50, num_classes) 
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+class CNN(nn.Module):
+    def __init__(self, in_channels=1, num_classes= 10):
+        super(CNN, self).__init__()
+        # Create Convolutional Layer:
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels, 
+            out_channels=8,
+            kernel_size = (3,3),
+            stride=(1,1),
+            padding=(1,1)
+        ) # Called a same convolution where the dimensions of the input and output images are the same
+        # Create a Pooling Layer:
+        self.pool = nn.MaxPool2d(
+            kernel_size=(2,2),
+            stride = (2,2)
+        )
+        # Create Convolutional Layer:
+        self.conv2 = nn.Conv2d(
+            in_channels=8, 
+            out_channels=16,
+            kernel_size = (3,3),
+            stride=(1,1),
+            padding=(1,1)
+        ) # Called a same convolution where the dimensions of the input and output images are the same
+        # Create fully connected Network Layer:
+        self.fc1 = nn.Linear(
+            16*7*7,
+            num_classes
+        )
     
-model = NN(784, 10) # Check the model to see if it runs correctly by using some random test data. 10 is for the number of digits. We want 10 values for each of the 784 images
-x = torch.randn(64, 784) # Number of examples to run simultaneously
-print(model(x).shape) # Returns the shape of the model
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
 
-
+        return x
 
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-#  Hyperparameters
-input_size = 784
+# Hyperparameters
+in_channel = 1
 num_classes = 10
 learning_rate = 0.001
 batch_size = 64
-num_epochs = 1
+num_epochs = 5
 
 # Load Data
 train_dataset = datasets.MNIST(root='dataset/', 
@@ -69,28 +95,27 @@ test_loader = DataLoader(
     shuffle = True
 )
 
-# Initialize network
+# Initialize Network
+model = CNN().to(device)
 
-model = NN(input_size, num_classes=num_classes).to(device)
 
-# Loss optimizer
+
+# Loss Optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(),
                        lr=learning_rate
                        )
 
-# Train Network
 
+# Train Network
 for epoch in range(num_epochs):
-    for batch_idx, (data, targets) in enumerate(train_loader):
+    for batch_idx, (data, targets) in enumerate(tqdm(train_loader)):
         
         # Get data to Cuda/gpu if possible
         data = data.to(device=device)
         targets = targets = targets.to(device = device)
-
-        data = data.reshape(data.shape[0], -1) # THe -1 unrolls the image to single dimension
         
-        print(data.shape) 
+       # print(data.shape) 
 
         # Foward
         scores = model(data)
@@ -112,8 +137,7 @@ for epoch in range(num_epochs):
         
         """
 
-# Check accuracy on training and test to see the accuracy of the model
-
+# Check Accuracy on training and test to see the accuracy of the model
 def check_accuracy(loader, model):
     if loader.dataset.train:
         print("Checking accuracy on training data")
@@ -127,7 +151,6 @@ def check_accuracy(loader, model):
         for x, y in loader:
             x = x.to(device=device)
             y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _,predictions = scores.max(1)
